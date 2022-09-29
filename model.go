8857bd8adaef6912e31aab0636dc768f5d056e0f
Jonathan Milhas <jmilhas@z2r5p3.42lyon.fr>
2022-09-29 02:55:03 +0200

    Feat: struct file

--- model.go
+++ model.go
@@ -4,33 +4,10 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/charmbracelet/bubbles/spinner"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
-
-type Model struct {
-	projectAdd     bool
-	projectRename  bool
-	projectList    *project
-	paginatorIndex int
-	spin           spinner.Model
-	search         textinput.Model
-	addbuffer      textinput.Model
-	renamebuffer   textinput.Model
-	searchValue    string
-	addValue       string
-	RenameValue    string
-	typing         bool
-	projectActive  bool
-	projet         lipgloss.Style
-	todoActive     bool
-	todo           lipgloss.Style
-	exitPopup      bool
-	DeletePopup    bool
-	err            error
-}
 
 func (m *Model) Init() tea.Cmd {
 	if m.typing || m.projectAdd || m.projectRename {
---
+++ struct.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/charmbracelet/bubbles/spinner"
+	"github.com/charmbracelet/bubbles/textinput"
+	"github.com/charmbracelet/lipgloss"
+)
+
+type Model struct {
+	projectAdd     bool
+	projectRename  bool
+	projectList    *project
+	paginatorIndex int
+	spin           spinner.Model
+	search         textinput.Model
+	addbuffer      textinput.Model
+	renamebuffer   textinput.Model
+	searchValue    string
+	addValue       string
+	RenameValue    string
+	typing         bool
+	projectActive  bool
+	projet         lipgloss.Style
+	todoActive     bool
+	todo           lipgloss.Style
+	exitPopup      bool
+	DeletePopup    bool
+	err            error
+}
